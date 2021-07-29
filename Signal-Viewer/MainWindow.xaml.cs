d27b1a3a6c24b3bae5fe70aefc14506ae578976c
Volodymyr <oho.user.v@gmail.com>
2021-07-29 18:59:58 +0300

    Created basic project, user application.

---
+++ Signal-Viewer/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Signal_Viewer {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
+
+/* -+-+-+- end file -+-+-+- */
